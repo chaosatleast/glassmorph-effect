5791c5ba51bec1d0e390afb67b5c5e11d38706b8
acxy99 <alicechin9017@gmail.com>
2024-12-27 11:41:15 +0800

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 import dynamic from "next/dynamic";
 
-const Tree = dynamic(() => import("@/components/React3Fiber/Tree"), {
-    ssr: false,
-});
-
 export default function Home() {
-    return (
-        <div className="relative h-full w-full">
-            <Tree />
-        </div>
-    );
+    return <div className="relative h-full w-full"></div>;
 }
